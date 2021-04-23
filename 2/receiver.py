f75bf98c9198da0bed16ed98098d925828677b12
kimia sharifi <kimiyasharifi72@gmail.com>
2021-04-23 13:52:01 +0430

    rabbitmq work queue (sender/receiver)

--- 2/receiver.py
+++ 2/receiver.py
@@ -11,12 +11,16 @@ print('waiting for message')
 
 def callback(ch, method, properties, body):
     print(f'received {body}')
+    print(method)
+    # sending another properties with message
+    print(properties.headers)
     time.sleep(6)
     print('Done!')
+    #mannual ack 
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
 # for granular sent 
 ch.basic_qos(prefetch_count=1)
-ch.basic_consume(queue='first', on_message_callback=callback, body)
+ch.basic_consume(queue='first', on_message_callback=callback)
 ch.start_consuming()
--- 2/sender.py
+++ 2/sender.py
@@ -9,6 +9,7 @@ ch = connection.channel()
 ch.queue_declare(queue='first', durable=True) #durable=true ---> queues are persisted to disk (related to only queue in channels)
 message = "this is testing message"
 
-ch.basic_publish(exchange='', routing_key='first', body=message, properties=pika.BasicProperties(delivery_mode=2,))
+ch.basic_publish(exchange='', routing_key='first', body=message, properties=pika.BasicProperties(delivery_mode=2, headers={'name':"kimia sharifi"}))     # sending another properties with message
+
 print('sent message')
 connection.close()
