3ff99e53853f9381945ac2710de7289f03960406
kimia sharifi <kimiyasharifi72@gmail.com>
2021-04-23 13:51:12 +0430

    command_of_rabbtimq

---
+++ 2/sender.py
@@ -0,0 +1,14 @@
+import  pika
+
+
+#create connection
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+#create channel
+ch = connection.channel()
+
+ch.queue_declare(queue='first', durable=True) #durable=true ---> queues are persisted to disk (related to only queue in channels)
+message = "this is testing message"
+
+ch.basic_publish(exchange='', routing_key='first', body=message, properties=pika.BasicProperties(delivery_mode=2,))
+print('sent message')
+connection.close()
